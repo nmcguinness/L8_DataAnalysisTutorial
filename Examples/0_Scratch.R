246f152e870d0ddf01fb7de22af3feaaf199dc85
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-03-01 11:43:18 +0000

    :sparkles: Added scratch file

---
+++ Examples/0_Scratch.R
@@ -0,0 +1,26 @@
+# this is where we add our R code
+
+# some data types
+age <- 25
+year <- 2023
+name <- "niall"
+college <- 'DkIT'
+isRaining <- TRUE
+isSummer <- FALSE
+height <- 1.9
+games <- c("LOL", "TLOU", "TR", "SMM")
+
+# viewing data in the console
+age
+games
+
+# forming strings with variables
+strAge <- paste("My age is", age)
+strAge
+
+print(paste("My age is", age))
+
+
+
+
+
